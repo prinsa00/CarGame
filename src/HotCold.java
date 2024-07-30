f74279ea586bbd0aefb6216da2afef546b30ced5
Prinsa <prinsashrestha979@gmail.com>
2024-07-30 13:10:27 +0545

    This is a  program to guess a number.

--- src/HotCold.java
+++ src/HotCold.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class HotCold {
     public static void main(String[]args){
-        
+        Scanner input = new Scanner(System.in);
+        int firstnumber = 345;
+
+        for(int i = 0; i <= 1000; i++){
+            System.out.println("Guess the number");
+            int num = input.nextInt();
+            if(num < firstnumber){
+                System.out.println("Cold");
+            }
+            else if(num > firstnumber){
+                System.out.println("Hot");
+            }
+            else{
+                System.out.println("The guess is correct.");
+                break;
+            }
+        }
+
+
+
+
+//        while(true){
+//            if(100>=n){
+//                System.out.println("Hot");
+//            }
+//            else if(1<=n){
+//                System.out.println("Cold");
+//            }
+//        }
     }
 }
